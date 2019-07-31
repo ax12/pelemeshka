2343b029e502e2e283cb80082c682f2368a8c2f2
Max <2903186@gmail.com>
2019-07-31 21:19:34 +0500

    social icons and font awesome

--- footer.php
+++ footer.php
@@ -14,9 +14,38 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-	
-		</div><!-- .site-info -->
+      <div class="row justify-content-md-center text-center d-flex justify-content-center">
+
+        <div class="col-md-3 col-sm-12 ">
+            <h4>Кафе «ВремЯ есть»</h4>
+            <img src="/wp-content/uploads/2018/11/foto-cafe-300x200.jpg" alt="">
+        </div>
+
+          <div class="col-md-3 col-sm-12 ">
+              <h4>Как нас найти</h4>
+              <img src="<?php echo carbon_get_theme_option('main_logo'); ?>" alt=""><p class="font-white">
+                Мы находимся в Пионерском районе, по адресу: Екатеринбург, ул. Советская, 55
+                Столик можно заказать по телефону: <br>
+                <?php echo carbon_get_theme_option('phone'); ?>
+            </p>
+        </div>
+
+        <div class="col-md-3 col-sm-12 links-footer">
+            <h4>Наши услуги</h4>
+
+                <a href="/biznes-lanch-v-nashem-kafe/">Бизнес ланч</a><br>
+                <a href="/arenda-banketnogo-zala/">Аренда банкетного зала</a><br>
+                <a href="/provedenie-korporativov/">Проведение корпоративов</a><br>
+                <a href="/dostavka/">Доставка пельменей</a><br>
+                <a href="/#zakazat">Забронировать столик</a><br>
+
+        </div>
+          <div class="col-md-3 col-sm-12 ">
+              <h4>Следуйте за нами</h4>
+              <i class="fab fa-instagram"></i> Instagramm
+          </div>
+
+		</div><!-- .end-row -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
